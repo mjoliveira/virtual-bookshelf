72e7dbd3b5cdd50fd10d3570b4b1f2da3640768b
mjoliveira <mayara.jesus@acad.pucrs.br>
2025-10-07 01:51:00 -0300

    feat: adds new methods to bookService

--- src/services/bookService.tsx
+++ src/services/bookService.tsx
@@ -1,8 +1,8 @@
-// src/services/bookService.js
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebase";
+import { Book } from "../types/book";
 
-// Função para salvar um livro
+// Adiciona um livro ao Firestore
 export const addBook = async (book:any) => {
   try {
     const docRef = await addDoc(collection(db, "books"), book);
@@ -10,5 +10,30 @@ export const addBook = async (book:any) => {
     return docRef.id;
   } catch (e) {
     console.error("Erro ao adicionar livro: ", e);
+  }
+};
+
+// Busca todos os livros cadastrados
+export const getBooks = async (): Promise<Book[]> => {
+  try {
+    const querySnapshot = await getDocs(collection(db, "books"));
+    const books: Book[] = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...(doc.data() as Omit<Book, "id">),
+    }));
+    return books;
+  } catch (error) {
+    console.error("Erro ao buscar livros:", error);
+    return [];
+  }
+};
+
+// Deleta um livro pelo ID
+export const deleteBook = async (id: string) => {
+  try {
+    await deleteDoc(doc(db, "books", id));
+    console.log("Livro deletado com ID:", id);
+  } catch (error) {
+    console.error("Erro ao deletar livro:", error);
   }
 };
